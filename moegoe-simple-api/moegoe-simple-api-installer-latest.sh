861ad6426603b681b283c01d298ef64750dd5c7d
Artrajz <969242373@qq.com>
2023-04-12 17:46:50 +0800

    Add files via upload

--- moegoe-simple-api/moegoe-simple-api-installer-latest.sh
+++ moegoe-simple-api/moegoe-simple-api-installer-latest.sh
@@ -1,19 +1,25 @@
 INSTALL_DIR=/usr/local/moegoe-simple-api
+
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PLAIN='\033[0m'
+
 mkdir -p $INSTALL_DIR
 cd $INSTALL_DIR
 if [ ! -f config.py ]; then
     echo -e "${YELLOW}下载config.py\n${PLAIN}"
     wget -O $INSTALL_DIR/config.py https://raw.githubusercontent.com/Artrajz/archived/main/moegoe-simple-api/config.py
 fi
+
 wget -O $INSTALL_DIR/docker-compose.yaml https://raw.githubusercontent.com/Artrajz/archived/main/moegoe-simple-api/docker-compose.yaml
+
 echo -e "${YELLOW}即将开始拉取镜像${PLAIN}"
 echo -e "${YELLOW}镜像大小为5g，拉取会比较慢\n${PLAIN}"
+
 docker compose pull
 docker compose up -d
+
 echo -e "\n已成功升级/安装MoeGoe-simple-api"
 echo -e "配置文件目录为 $(realpath $INSTALL_DIR)"
 echo -e "${RED}2023.4.7更新-需要更新config.py，否则会报错${PLAIN}"
